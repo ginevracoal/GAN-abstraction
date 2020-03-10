da7b90bb52cbe16addba4b9442e66a8b7a3f9003
ginevracoal <ginevracoal@gmail.com>
2020-03-10 17:16:06 +0100

    added embed and fixed_params options

--- GAN/src/exec.sh
+++ GAN/src/exec.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-N_TRAJ=30000
+MODEL="eSIR"  # SIR, eSIR, Repress, Toggle
 TIMESTEPS=128
-NOISE_TIMESTEPS=5
-BATCH_SIZE=128
-MODEL="SIR"  # SIR, eSIR, Repress, Toggle
-EPOCHS=300
-GEN_EPOCHS=10
+NOISE_TIMESTEPS=128
+BATCH_SIZE=512
+EPOCHS=50
+GEN_EPOCHS=20
+EMBED=False
+FIXED_PARAMS=True
 
 ### launch from anywhere on server
 cd ~/GAN-abstraction/GAN/src/
@@ -22,7 +23,8 @@ mkdir -p $RESULTS
 OUT="${RESULTS}${TIME}_out.txt"
 
 ## run script
-python3 gan_abstraction.py --n_traj=$N_TRAJ --timesteps=$TIMESTEPS --noise_timesteps=$NOISE_TIMESTEPS --batch_size=$BATCH_SIZE --model=$MODEL --epochs=$EPOCHS --gen_epochs=$GEN_EPOCHS &> $OUT
+python3 gan_abstraction.py --n_traj=30000 --timesteps=$TIMESTEPS --noise_timesteps=$NOISE_TIMESTEPS --batch_size=$BATCH_SIZE --model=$MODEL --epochs=$EPOCHS --gen_epochs=$GEN_EPOCHS --embed=$EMBED --fixed_params=$FIXED_PARAMS&> $OUT
+python3 gan_evaluation.py --n_traj=2000 --timesteps=$TIMESTEPS --noise_timesteps=$NOISE_TIMESTEPS --model=$MODEL --epochs=$EPOCHS --gen_epochs=$GEN_EPOCHS &> $OUT
 
 ## deactivate venv
 deactivate
