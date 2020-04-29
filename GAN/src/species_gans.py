93a06f281217f7b9cab8a2668c054611a79a917b
ginevracoal <ginevracoal@gmail.com>
2020-04-29 18:35:39 +0200

    testing new implementation

--- GAN/src/species_gans.py
+++ GAN/src/species_gans.py
@@ -10,11 +10,100 @@ from utils import load_from_pickle, execution_time, generate_noise, save_to_pick
 import time
 from directories import *
 import os
+import random
 import itertools
 from gan_evaluation import GAN_evaluator
 
 
-class SpeciesGANs(GAN_abstraction):
+class batches_GAN_abstraction(GAN_abstraction):
+    def __init__(self, model, timesteps, noise_timesteps, fixed_params, gen_lr, discr_lr, 
+                  n_epochs, gen_epochs):
+            super(batches_GAN_abstraction, self).__init__(model, timesteps, noise_timesteps, 
+                  fixed_params, gen_lr, discr_lr, n_epochs, gen_epochs)
+
+    def generate_real_samples(self, training_data):
+        t, s, p = training_data 
+        return [t,s] if self.fixed_params==1 else [t,s,p]
+
+    def generate_latent_samples(self, training_data):
+        _, s, p = training_data 
+        noise_shape = (len(training_data[0]), self.noise_timesteps, self.n_species)
+        noise = np.random.normal(loc=0., scale=10., size=noise_shape)
+        return [noise,s] if self.fixed_params==1 else [noise,s,p]
+
+    def generate_fake_samples(self, training_data, generator):
+        latent_data = self.generate_latent_samples(training_data)
+        _, s, p = latent_data
+        gen_traj = generator.predict(latent_data)
+        gen_traj = np.round(gen_traj)  
+        return [gen_traj,s] if self.fixed_params==1 else [gen_traj,s,p]
+
+    def train(self, training_data, batch_size, seed=0):
+        random.seed(seed)
+
+        generator = self.generator()
+        discriminator = self.discriminator()
+        gan = self.gan(discriminator, generator)
+
+        g_loss=0
+        d_loss1=0
+        d_loss2=0
+        d_acc1=0
+        d_acc2=0
+
+        g_loss_list=[]
+        d_loss1_list=[]
+        d_loss2_list=[]
+        d_acc1_list=[]
+        d_acc2_list=[]
+
+        n_batches = int(len(training_data[0])/batch_size)
+        half_batch = int(batch_size*0.5)
+
+        start = time.time()
+        for epoch in range(self.n_epochs):
+            for i in range(n_batches):
+
+                x_batch = [data[i:i+batch_size] for data in training_data]
+
+                # == d1 training ==
+                first_half_batch = [data[:half_batch] for data in x_batch]
+                x_train_real = self.generate_real_samples(first_half_batch)
+                y_train_real = np.ones(len(x_train_real[0]))
+                d_loss1, d_acc1 = discriminator.train_on_batch(x_train_real, y_train_real)
+
+                # == d2 training ==
+                second_half_batch = [data[half_batch:] for data in x_batch]
+                x_train_fake = self.generate_fake_samples(second_half_batch, generator)
+                y_train_fake = np.zeros(len(x_train_fake[0]))
+                d_loss2, d_acc2 = discriminator.train_on_batch(x_train_fake, y_train_fake)
+
+                # == g training ==
+                for _ in range(self.gen_epochs):
+                    x_latent = self.generate_latent_samples(x_batch)            
+                    y_train_real = np.ones(len(x_latent[0]))
+                    g_loss = gan.train_on_batch(x=x_latent, y=y_train_real)
+            
+            d_loss1_list.append(d_loss1)
+            d_loss2_list.append(d_loss2)
+            g_loss_list.append(g_loss)
+            d_acc1_list.append(d_acc1*100)
+            d_acc2_list.append(d_acc2*100)   
+
+            print(f"\n[Epoch {epoch + 1}]\t g_loss = {g_loss:.4f}", end="\t")
+            print(f"d_loss1 = {d_loss1:.4f}\td_loss2 = {d_loss2:.4f}", end="\t")
+            print(f"a1 = {int(100*d_acc1)}\ta2 = {int(100*d_acc2)}", end="\t")
+
+        print("\n")
+        execution_time(start=start, end=time.time())
+
+        self.fig = self.plot_training(g_loss_list, d_loss1_list, d_loss2_list, d_acc1_list, 
+                                       d_acc2_list)
+
+        return discriminator, generator
+
+
+class SpeciesGANs(batches_GAN_abstraction):
     def __init__(self, model, timesteps, noise_timesteps, fixed_params, gen_lr, discr_lr, n_epochs, 
                  gen_epochs):
         super(SpeciesGANs, self).__init__(model, timesteps, noise_timesteps, fixed_params, gen_lr, 
@@ -34,7 +123,7 @@ class SpeciesGANs(GAN_abstraction):
             print("\n === Training on specie ", idx, "===")
             specie_data = [np.expand_dims(data[:,:,idx],2) for data in training_data]
 
-            classifier = GAN_abstraction(model=self.model, fixed_params=self.fixed_params,
+            classifier = batches_GAN_abstraction(model=self.model, fixed_params=self.fixed_params,
                           timesteps=self.timesteps, noise_timesteps=self.noise_timesteps, 
                           discr_lr=self.discr_lr, gen_lr=self.gen_lr,
                           n_epochs=self.n_epochs, gen_epochs=self.gen_epochs)
@@ -71,10 +160,11 @@ class SpeciesGANs(GAN_abstraction):
             classifier.architecture = "conv1D"
             discr = keras.models.load_model(path+"discriminator_specie="+str(idx)+".h5")
             gen = keras.models.load_model(path+"generator_specie="+str(idx)+".h5") 
-            
             species_gans.update({str(idx):[discr,gen]})
 
         return species_gans
+
+   
 
 
 class SpeciesEval(GAN_evaluator):
@@ -161,8 +251,8 @@ def main(args):
                       timesteps=args.timesteps, noise_timesteps=args.noise_timesteps, 
                       discr_lr=args.discr_lr, gen_lr=args.gen_lr,
                       n_epochs=args.epochs, gen_epochs=args.gen_epochs)
-    training_data = gan.load_data(n_traj=args.traj, model=args.model, timesteps=args.timesteps)
-    
+    training_data = gan.load_data(n_traj=args.traj, model=args.model, timesteps=args.timesteps,
+                                  shuffle=True)
     species_gans = gan.train(training_data=training_data,  batch_size=args.batch_size)
     # species_gans = gan.load(rel_path=RESULTS)
 
@@ -175,7 +265,7 @@ def main(args):
 
     test_data = species_eval.load_test_data(model=args.model)
     traj = species_eval.compute_trajectories(species_gans=species_gans, test_data=test_data)
-    species_eval.save_trajectories(data=traj)
+    # species_eval.save_trajectories(data=traj)
     # traj = species_eval.load_trajectories(rel_path=RESULTS)
     species_eval.plot_trajectories(trajectories=traj, n_traj=args.eval_traj)
 
@@ -187,7 +277,7 @@ if __name__ == "__main__":
     parser.add_argument("--eval_traj", default=10, type=int)
     parser.add_argument("--batch_size", default=128, type=int)
     parser.add_argument("--timesteps", default=32, type=int)
-    parser.add_argument("--noise_timesteps", default=128, type=int)
+    parser.add_argument("--noise_timesteps", default=32, type=int)
     parser.add_argument("--epochs", default=10, type=int)
     parser.add_argument("--gen_epochs", default=2, type=int)
     parser.add_argument("--fixed_params", default=0, type=int)
