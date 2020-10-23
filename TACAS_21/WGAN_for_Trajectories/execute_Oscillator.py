11efaf8ff70d25abacd1a28486409efd2fdd9d60
Francesca Cairoli <francesca.cairoli@studenti.units.it>
2020-10-23 16:51:18 +0200

    adding TACAS submitted version

---
+++ TACAS_21/WGAN_for_Trajectories/execute_Oscillator.py
@@ -0,0 +1,30 @@
+from execute import *
+
+model_name = "Oscillator"
+labels = ["A", "B", "B"]
+colors = ["b", "r", "g"]
+
+noise_dim = 480
+state_dim = 3
+traj_len = 32
+
+n_epochs = 1
+batch_size = 256
+n_critic = 1
+n_gen = 1
+
+
+do_train_flag = False
+
+if do_train_flag:
+	execute(model_name, state_dim, traj_len, 
+			labels, colors, noise_dim = noise_dim, n_epochs = n_epochs, 
+			batch_size = batch_size, n_critic = n_critic, n_gen = n_gen)
+else:
+	trained_model = "/final_generator_100_epochs.h5"
+	model_id = "TACAS"
+	evaluate_trained_model(trained_model, model_id, model_name, state_dim, traj_len, 
+			labels, colors, noise_dim = noise_dim, n_epochs = n_epochs, 
+			batch_size = batch_size, n_critic = n_critic, n_gen = n_gen)
+
+	
---
+++ TACAS_21/WGAN_for_Trajectories/execute_ToggleSwitch.py
@@ -0,0 +1,30 @@
+from execute import *
+
+model_name = "ToggleSwitch"
+labels = ["P1", "P2"]
+colors = ["b", "r"]
+
+noise_dim = 960
+state_dim = 2
+traj_len = 32
+
+n_epochs = 1
+batch_size = 256
+n_critic = 5
+n_gen = 1
+
+
+do_train_flag = False
+
+if do_train_flag:
+	execute(model_name, state_dim, traj_len, 
+			labels, colors, noise_dim = noise_dim, n_epochs = n_epochs, 
+			batch_size = batch_size, n_critic = n_critic, n_gen = n_gen)
+else:
+	trained_model = "/final_generator_400_epochs.h5"
+	model_id = "TACAS"
+	evaluate_trained_model(trained_model, model_id, model_name, state_dim, traj_len, 
+			labels, colors, noise_dim = noise_dim, n_epochs = n_epochs, 
+			batch_size = batch_size, n_critic = n_critic, n_gen = n_gen)
+
+	
